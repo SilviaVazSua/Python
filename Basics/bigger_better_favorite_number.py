df4941549907641e6ff0adc8a2d572b499ba7813
Silvia Vázquez <41201525+SilviaVazSua@users.noreply.github.com>
2018-11-06 12:59:13 -0800

    create
    bigger_better_favorite_number.py

---
+++ Basics/bigger_better_favorite_number.py
@@ -0,0 +1,5 @@
+# This program asks for a person’s favorite number. It adds 1 to the number, and then suggests the result as a bigger and better favorite number.
+
+fav_number = input("Tell me, what\'s your favourite number? ")
+fav_number2 = int(fav_number) + 1
+print("Then... if we add 1, you can have your bigger and better favourite number:" , fav_number2 , "!!!")
