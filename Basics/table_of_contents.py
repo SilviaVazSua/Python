7450b99dec285400ae28ddae7f78d293f4ebe789
Silvia Vázquez <41201525+SilviaVazSua@users.noreply.github.com>
2018-11-06 13:03:26 -0800

    create table_of_contents.py

---
+++ Basics/table_of_contents.py
@@ -0,0 +1,6 @@
+# This program displays a table of contents
+
+print("Table of Contents".center(10), "\n")
+print("Chapter 1: Getting Started".ljust(0), "page 1".rjust(40))
+print("Chapter 2: Numbers".ljust(0), "page 9".rjust(48))
+print("Chapter 3: Letters".ljust(0), "page 13".rjust(49))
