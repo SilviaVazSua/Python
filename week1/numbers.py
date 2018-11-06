f7ccf7696a533e25938813b1e9b956cccb9ec4d1
Silvia Vázquez <41201525+SilviaVazSua@users.noreply.github.com>
2018-11-06 12:42:17 -0800

    update numbers.py

--- week1/numbers.py
+++ week1/numbers.py
@@ -11,3 +11,16 @@ age_seconds = 60 * 60 * 24 * 365 * 33
 
 # Andreea is 48618000 seconds old. It calculates Andreea's age.
 andreea = 48618000 / 60 / 60 / 24 / 365
+
+
+print("How many hours are in a year? ")
+print("Easy: ", hours_year)
+
+print("And how many minutes in decade? ")
+print("Easy: ", minutes_decade)
+
+print("If I'm 33 years old, how many second old am I? ")
+print("Easy: ", age_seconds)
+
+print("If Andreea is 48618000 seconds old, how old is she in years? ")
+print("Easy: ", andreea, ". So young!!! :D ")
