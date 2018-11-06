2a55764fbb39166f82b3194cd8802b2dea4bac44
Silvia Vázquez <41201525+SilviaVazSua@users.noreply.github.com>
2018-11-06 13:02:06 -0800

    create angry_boss.py

---
+++ Basics/angry_boss.py
@@ -0,0 +1,4 @@
+# This is an angry boss program that rudely asks what you want. Whatever you answer, the angry boss should yell it back to you and then fire you. For example, if you type in I want a raise, it should yell back like this: 'WHADDAYA MEAN "I WANT A RAISE"?!? YOU'RE FIRED!!'
+
+what_you_want = input("WHAT THE HELLL YOU WANT NOOOOOW?? I'M WORKING!!! ")
+print('WHADDAYA MEAN "'+ what_you_want +'"?!? YOU\'RE FIRED!!')
