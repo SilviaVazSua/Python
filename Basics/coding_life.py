0ee4b7ac676ac07bca5c4fea00f552201625db84
Silvia Vázquez <41201525+SilviaVazSua@users.noreply.github.com>
2018-11-06 13:11:34 -0800

    create coding_life.py

---
+++ Basics/coding_life.py
@@ -0,0 +1,9 @@
+# a program about something in real life :D
+
+
+number_of_stairs_1_2 = int(input("Tell me how many stairs from floor 1 to floor 2, please: "))
+floor = int(input("In which floor you live? "))
+
+total_stairs = number_of_stairs_1_2 * floor
+
+print("Then, if the elevator doesn\'t work, you will have ", total_stairs, "until your home! Maybe you want to stay at a friend's home! :D")
